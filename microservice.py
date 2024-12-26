ed163296ac650e9a279327b833ea65267da92b70
aviran <aviran@gmail.com>
2024-12-26 18:55:03 +0200

    bump connexion version

--- microservice.py
+++ microservice.py
@@ -2,18 +2,19 @@ import logging.config
 import os
 import connexion
 from config.logging import LOGGING_CONFIG
+from connexion.options import SwaggerUIOptions
 
 logging.config.dictConfig(LOGGING_CONFIG)
 
 LOCAL_PORT = int(os.environ.get('MICROSERVICE_PORT', 0)) or 8080
 
-app = connexion.FlaskApp(__name__, specification_dir="spec")
-app.add_api("openapi.yaml")
+options = SwaggerUIOptions(swagger_ui_path="/docs")
+app = connexion.AsyncApp(__name__, specification_dir="spec", swagger_ui_options=options)
+
+# Swagger-UI will be /openapi/docs - the openapi prefix comes from the spec file.
+app.add_api("openapi.yaml", swagger_ui_options=options)
+
 
 if __name__ == "__main__":
-
     # Development only server start
-    app.options.extend({"swagger_ui": True})
-    app.run(debug=True,
-            host='0.0.0.0',
-            port=LOCAL_PORT)
+    app.run(host='0.0.0.0', port=LOCAL_PORT)
